01ebea9ab5d6502282849c77dd335b28be5f8a15
shitieshen-86-0594 <18750032048@163.com>
2026-08-16 14:55:15 +0800

    Create Color.kt

---
+++ app/src/main/java/com/example/audioconverter/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.audioconverter.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GlassPrimary = Color(0xFF7B68EE)
+val GlassSecondary = Color(0xFF00D2FF)
+val GlassBackground = Color(0xFF0F0F1A)
+val GlassSurface = Color(0x1AFFFFFF)
+val GlassBorder = Color(0x33FFFFFF)
+val GlassTextPrimary = Color(0xFFF0F0F5)
+val GlassTextSecondary = Color(0xB3FFFFFF)
